31830e1fd4a74074566b4a831dfec5e26f1494a8
Mike Mitterer <office@mikemitterer.at>
2015-02-20 17:35:46 +0100

    reorg: Animation and Button get their own pubspec

---
+++ lib/wsk_dialog/utils.dart
@@ -0,0 +1,63 @@
+part of wsk_angular.wsk_dialog;
+
+/// Creates html.Element from {htmlString}. This {htmlString} must contain wsk-dialog that defines the
+/// root-Element for this Dialog
+html.Element _createDialogElementFromString(final String htmlString,{ final String rootTag: "wsk-dialog" }) {
+    Validate.notBlank(htmlString);
+    Validate.isTrue(htmlString.contains(rootTag),"The HTML-String must contain '${rootTag}'!");
+
+    final html.HtmlElement baseElement = new html.DivElement();
+    baseElement.setInnerHtml(htmlString, treeSanitizer: new NullTreeSanitizer());
+
+    final List<html.Node> nodes = new List<html.Node>();
+    for (final html.Node node in baseElement.nodes) {
+        if (node is html.Element) {
+            if ((node as html.Element).tagName.toLowerCase() == rootTag) {
+                return node as html.Element;
+            }
+        }
+    }
+    throw new ArgumentError("Could not find <${rootTag}>...</${rootTag}> in '$htmlString'!");
+}
+
+final Logger _logger = new Logger('wsk_angular.wsk_dialog._waitForComponentToLoad');
+Future<html.HtmlElement> _waitForComponentToLoad(final String selector, { Completer completer,
+html.Element parent, final int inMilliSeconds: 10, final int TIMEOUT_IN_MS: 1000 }) {
+
+    Validate.notBlank(selector);
+
+    if (completer == null) {
+        completer = new Completer<html.HtmlElement>();
+    }
+    if (parent == null) {
+        parent = html.document.querySelector("body");
+    }
+
+    if (inMilliSeconds >= TIMEOUT_IN_MS) {
+        throw new TimeoutException("Could not find an element with: ${selector} as selector!");
+    }
+
+    _logger.finer("Next check for component - in: ${inMilliSeconds}ms");
+    new Future.delayed(new Duration(milliseconds: inMilliSeconds), () {
+        _logger.finer(" - Selector: .${selector}");
+
+        html.HtmlElement component = parent.querySelector(selector);
+        if (component == null) {
+            throw "Element with ${selector} not ready yet, try it again...";
+        }
+        _logger.fine("Found: $component with selector: ${selector}");
+        return component;
+
+    }).then((final html.HtmlElement component) {
+        completer.complete(component);
+
+    }).catchError((_) {
+
+        // next round...
+        _waitForComponentToLoad(selector, completer: completer,
+        parent: parent, inMilliSeconds: inMilliSeconds < 100 ? inMilliSeconds + 5 : inMilliSeconds * 2);
+
+    });
+
+    return completer.future;
+}
--- lib/wsk_dialog/wsk_dialog.dart
+++ lib/wsk_dialog/wsk_dialog.dart
@@ -12,6 +12,8 @@ import 'package:validate/validate.dart';
 import 'package:wsk_material/wskcomponets.dart';
 import 'package:wsk_angular/wsk_angular.dart';
 
+part "utils.dart";
+
 /// Store strings for class names defined by this component that are used in
 /// Dart. This allows us to simply change it in one place should we
 /// decide to modify at a later date.
@@ -30,90 +32,11 @@ class _WskDialogCssClasses {
  */
 class WskDialogModule extends Module {
     WskDialogModule() {
-        //bind(WskDialogComponent);
-        //bind(WskDialog);
+
         bind(WskAlertDialog);
 
         //- Services ---------------------------
     }
-}
-
-///// WskDialogComponent
-//@Component(selector: 'wsk-dialog-xx', useShadowDom: false, templateUrl: 'packages/wsk_angular/wsk_dialog/wsk_dialog.html')
-//class WskDialogComponent {
-//    final Logger _logger = new Logger('wsk_angular.wsk_dialog.WskDialogComponent');
-//
-//    final html.Element _component;
-//
-//    WskDialogComponent(final html.Element component) : _component = component {
-//        Validate.notNull(component);
-//    }
-//
-//// - EventHandler -----------------------------------------------------------------------------
-//
-//// - private ----------------------------------------------------------------------------------
-//}
-
-/// Creates html.Element from {htmlString}. This {htmlString} must contain wsk-dialog that defines the
-/// root-Element for this Dialog
-html.Element _createDialogElementFromString(final String htmlString,{ final String rootTag: "wsk-dialog" }) {
-    Validate.notBlank(htmlString);
-    Validate.isTrue(htmlString.contains(rootTag),"The HTML-String must contain '${rootTag}'!");
-
-    final html.HtmlElement baseElement = new html.DivElement();
-    baseElement.setInnerHtml(htmlString, treeSanitizer: new NullTreeSanitizer());
-
-    final List<html.Node> nodes = new List<html.Node>();
-    for (final html.Node node in baseElement.nodes) {
-        if (node is html.Element) {
-            if ((node as html.Element).tagName.toLowerCase() == rootTag) {
-                return node as html.Element;
-            }
-        }
-    }
-    throw new ArgumentError("Could not find <${rootTag}>...</${rootTag}> in '$htmlString'!");
-}
-
-final Logger _logger = new Logger('wsk_angular.wsk_dialog._waitForComponentToLoad');
-Future<html.HtmlElement> _waitForComponentToLoad(final String selector, { Completer completer,
-        html.Element parent, final int inMilliSeconds: 10, final int TIMEOUT_IN_MS: 1000 }) {
-
-    Validate.notBlank(selector);
-
-    if (completer == null) {
-        completer = new Completer<html.HtmlElement>();
-    }
-    if (parent == null) {
-        parent = html.document.querySelector("body");
-    }
-
-    if (inMilliSeconds >= TIMEOUT_IN_MS) {
-        throw new TimeoutException("Could not find an element with: ${selector} as selector!");
-    }
-
-    _logger.finer("Next check for component - in: ${inMilliSeconds}ms");
-    new Future.delayed(new Duration(milliseconds: inMilliSeconds), () {
-        _logger.finer(" - Selector: .${selector}");
-
-        html.HtmlElement component = parent.querySelector(selector);
-        if (component == null) {
-            throw "Element with ${selector} not ready yet, try it again...";
-        }
-        _logger.fine("Found: $component with selector: ${selector}");
-        return component;
-
-    }).then((final html.HtmlElement component) {
-        completer.complete(component);
-
-    }).catchError((_) {
-
-        // next round...
-        _waitForComponentToLoad(selector, completer: completer,
-            parent: parent, inMilliSeconds: inMilliSeconds < 100 ? inMilliSeconds + 5 : inMilliSeconds * 2);
-
-    });
-
-    return completer.future;
 }
 
 enum WskDialogStatus { CLOSED_BY_ESC, CLOSED_BY_BACKDROPCLICK, OK }
